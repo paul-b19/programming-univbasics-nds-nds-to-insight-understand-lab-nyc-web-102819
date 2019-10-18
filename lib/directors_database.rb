a10c2c46b2c9defc23609e51ba10c6d1e2819f67
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-18 05:47:04 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -1,11 +1,12 @@
 require 'yaml'
-require 'pp'
+#require 'pp'
 
 def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
 
-def pretty_print_nds
-  pp
+def pretty_print_nds(nds)
+  require 'pp'
+  pp nds
 end
